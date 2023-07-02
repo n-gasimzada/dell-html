b58c288b6f9a4c7c52aaed964a47a8acfd8f9b49
ChocoChild <nijatgasimzada@gmail.com>
2023-07-02 15:03:08 +0400

    Шапка готова

---
+++ js/main.js
@@ -0,0 +1,7 @@
+const burgerBtn = document.querySelector('.header__burger-menu')
+const navBtn = document.querySelector('.nav__list')
+
+burgerBtn.addEventListener('click', function() {
+    burgerBtn.classList.toggle('active')
+    navBtn.classList.toggle('active')
+})
